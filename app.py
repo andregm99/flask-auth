06096c7b1aa56bc5cd1c086cae23cedc001be76f
andregm99 <gomes.andre11@hotmail.com>
2025-09-13 14:56:30 -0300

    adicionando sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+from models.user import User
+from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+#config do banco de dados.
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+login_manager= LoginManager()
+db.init_app(app)
+login_manager.init_app(app)
+#
+
+@app.route('/login', methods=["POST"])
+def login():
+  data = request.get_json()
+  username = data.get("username")
+  password = data.get("password")
+
+  if username and password:
+   # Login
+    pass
+
+  return jsonify({"message": "Credenciais inválidas"}), 400
+
+
 
 @app.route("/", methods=["GET"])
 def home():
